49799226805a7660d4e6a2d86d88c7e461cd5b88
Klaus <klaus.accso@ard.de>
2025-06-16 16:21:04 +0200

    Fix local and github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === "true";
 const repoName = "slack-clone";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/" + repoName,
-  assetPrefix: "/" + repoName + "/",
+  ...(isGithubPages && {
+    basePath: "/" + repoName,
+    assetPrefix: "/" + repoName + "/",
+  }),
   /* config options here */
 };
 
