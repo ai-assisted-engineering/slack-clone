5adc13836ca80a9e7540fd8d51bafef9e1be56cf
Klaus <klaus.accso@ard.de>
2025-06-17 18:32:09 +0200

    agent: add missing packages

--- db/index.ts
+++ db/index.ts
@@ -1,12 +1,13 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
-import { users } from './schema'
+
 
 const connectionString = process.env.DATABASE_URL
 
-// Disable prefetch as it is not supported for "Transaction" pool mode
-const client = postgres(connectionString, { prepare: false })
-const db = drizzle(client);
+const schema = { };
 
-const allUsers = await db.select().from(users);
-        
+// Disable prefetch as it is not supported for "Transaction" pool mode
+const client = postgres(connectionString, { prepare: false });
+
+export const db = drizzle(client, { schema });
+
