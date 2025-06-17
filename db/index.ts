394193ea71a7edd95c0e8a63bbda4b08b1ac9d63
Klaus <klaus.accso@ard.de>
2025-06-17 18:35:05 +0200

    human: init

--- db/index.ts
+++ db/index.ts
@@ -2,12 +2,16 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
 
-const connectionString = process.env.DATABASE_URL
+const databaseUrl = process.env.DATABASE_URL
+if (!databaseUrl) {
+    throw new Error("DATABASE_URL is not set")
+  }
+  
 
 const schema = { };
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
-const client = postgres(connectionString, { prepare: false });
+const client = postgres(databaseUrl, { prepare: false });
 
 export const db = drizzle(client, { schema });
 
