ff1b0cd857b270d4827a67d3bd8b9eabbbdd0a33
Klaus <klaus.accso@ard.de>
2025-06-22 10:09:31 +0200

    trigger deployment

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly. Can i see this env variable: {process.env.TEST}
+            Save and see your changes instantly. Can i see this env variable: {process.env.TEST}. This should be updated.
           </li>
         </ol>
 
